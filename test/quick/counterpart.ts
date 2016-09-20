09a4cf1e9f6a759634d6e89bf649d3cbfb39c315
Offirmo <offirmo.net@gmail.com>
2016-09-20 19:09:28 +0200

    +

---
+++ test/quick/counterpart.ts
@@ -0,0 +1,46 @@
+#!/bin/sh
+':' //# http://sambal.org/?p=1014 ; exec `dirname $0`/../../node_modules/.bin/ts-node "$0" "$@"
+'use strict';
+
+/// <reference path="../../typings-custom/counterpart.d.ts" />
+
+import { polyfill_intl_for_locales } from '../../src/node/auto-intl-polyfiller'
+
+console.log('Hello world !')
+
+polyfill_intl_for_locales([ 'en', 'fr'])
+
+//import * as counterpart from 'counterpart'
+const Counterpart = require('counterpart')
+
+Counterpart.registerTranslations('fr', {
+	hello: 'Bonjour',
+	x_items: {
+		zero:  'Pas d’éléments.',
+		one:   'Un élément.',
+		other: '%(count)s éléments.'
+	}
+})
+
+Counterpart.registerTranslations('en', {
+	hello: 'Hello',
+	x_items: {
+		zero:  'No items.',
+		one:   'One item.',
+		other: '%(count)s items.'
+	}
+})
+
+console.log('*** fr ***')
+Counterpart.setLocale('fr')
+console.log(Counterpart.translate('hello'))
+console.log(Counterpart.translate('x_items', { count: 0  }))
+console.log(Counterpart.translate('x_items', { count: 1  }))
+console.log(Counterpart.translate('x_items', { count: 42 }))
+
+console.log('*** en ***')
+Counterpart.setLocale('en')
+console.log(Counterpart.translate('hello'))
+console.log(Counterpart.translate('x_items', { count: 0  }))
+console.log(Counterpart.translate('x_items', { count: 1  }))
+console.log(Counterpart.translate('x_items', { count: 42 }))
---
+++ typings-custom/counterpart.d.ts
@@ -0,0 +1,23 @@
+declare namespace CounterpartModule {
+  type _CounterpartConstructor = {
+    prototype: Counterpart
+  }
+  type _LocaleChangedHandler = (locale: string) => void
+  type _Translations = { [key: string]: string }
+}
+declare interface Counterpart {
+  registerTranslations(locale: string, translations: CounterpartModule._Translations): void
+  translate(key: string, options?: Object): string
+  setLocale(nextLocale: string): string // returns previousLocale
+  getLocale(): string
+  onLocaleChange(handler: CounterpartModule._LocaleChangedHandler): void
+  offLocaleChange(handler: CounterpartModule._LocaleChangedHandler): void
+}
+declare module 'counterpart' {
+  interface TranslateFunction extends Counterpart {
+    (key: string, params?: {}): string
+    Instance: CounterpartModule._CounterpartConstructor
+  }
+  const translate: TranslateFunction
+  export default translate
+}
